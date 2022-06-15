f6aec0bf709f21d4c2378a40cc4bdc48507a33dd
WiyanEl <wiyanbarkah92@gmail.com>
2022-06-15 23:36:27 +0700

    Perbarui portfolio versi 2.0.0

--- src/script/index.js
+++ src/script/index.js
@@ -1,8 +1,102 @@
-import {navLinkToggle, navbarLoad, navbarResize, navbarScroll} from "./navbar.js";
-import "./scroll-section.js";
-import "./parallax.js";
+(function () {
+  "use strict";
 
-navLinkToggle();
-navbarLoad();
-navbarResize();
-navbarScroll();
+  const select = (el, all = false) => {
+    el = el.trim();
+    if (all) {
+      return [...document.querySelectorAll(el)];
+    } else {
+      return document.querySelector(el);
+    }
+  };
+
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all);
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach((e) => e.addEventListener(type, listener));
+      } else {
+        selectEl.addEventListener(type, listener);
+      }
+    }
+  };
+
+  const onscroll = (el, listener) => {
+    el.addEventListener("scroll", listener);
+  };
+
+  // Mobile toggle
+  on("click", ".mobile-nav-toggle", function (event) {
+    select("#navbar").classList.toggle("navbar-mobile");
+    this.classList.toggle("bi-list");
+    this.classList.toggle("bi-x");
+  });
+
+  on("click", ".navbar .dropdown > a", function (event) {
+    if (select("#navbar").classList.contains("navbar-mobile")) {
+      event.preventDefault();
+      this.nextElementSibling.classList.toggle("dropdown-active");
+    }
+  });
+
+  // Toggle active state on scroll
+  let navbarlinks = select("#navbar .scrollto", true);
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200;
+    navbarlinks.forEach((navbarlink) => {
+      if (!navbarlink.hash) return;
+      let section = select(navbarlink.hash);
+      if (!section) return;
+      if (position >= section.offsetTop && position <= section.offsetTop + section.offsetHeight) {
+        navbarlink.classList.add("active");
+      } else {
+        navbarlink.classList.remove("active");
+      }
+    });
+  };
+  window.addEventListener("load", navbarlinksActive);
+  onscroll(document, navbarlinksActive);
+
+  // Scroll events
+  const scrollTo = (el) => {
+    let header = select("#header");
+    let offset = header.offsetHeight;
+
+    if (!header.classList.contains("header-scrolled")) {
+      offset -= 16;
+    }
+
+    let elementPos = select(el).offsetTop;
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: "smooth",
+    });
+  };
+
+  let selectHeader = select("#header");
+  if (selectHeader) {
+    const headerScrolled = () => {
+      if (window.scrollY > 100) {
+        selectHeader.classList.add("header-scrolled");
+      } else {
+        selectHeader.classList.remove("header-scrolled");
+      }
+    };
+    window.addEventListener("load", headerScrolled);
+    onscroll(document, headerScrolled);
+  }
+
+  // Typed items
+  const typed = select(".typed");
+  if (typed) {
+    let typed_strings = typed.getAttribute("data-typed-items");
+    typed_strings = typed_strings.split(",");
+    new Typed(".typed", {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 100,
+      backSpeed: 50,
+      backDelay: 2000,
+    });
+  }
+})();
